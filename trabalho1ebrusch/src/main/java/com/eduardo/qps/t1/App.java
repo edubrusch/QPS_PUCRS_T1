db1557dd840ba45e34dac2681d26c7e02669951a
edubrusch <edubrusch@gmail.com>
2017-09-11 22:02:41 -0300

    criacao da estrutura

---
+++ trabalho1ebrusch/src/main/java/com/eduardo/qps/t1/App.java
@@ -0,0 +1,13 @@
+package com.eduardo.qps.t1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ trabalho1ebrusch/src/test/java/com/eduardo/qps/t1/ScoresTest.java
@@ -0,0 +1,7 @@
+package com.eduardo.qps.t1;
+
+import junit.framework.TestCase;
+
+public class ScoresTest extends TestCase {
+
+}
